a02942746c3d9c0837fcc676ffd17ccbbdccdc9d
yangmin <641077035@qq.com>
2017-03-07 17:18:50 +0800

    通过UDP，USB、WiFi接收的数据流处理

---
+++ EADemo/EADSessionController.h
@@ -0,0 +1,32 @@
+/*
+ Copyright (C) 2016 Apple Inc. All Rights Reserved.
+ See LICENSE.txt for this sample’s licensing information
+ 
+ Abstract:
+ Provides an interface for communication with an EASession. Also the delegate for the EASession input and output stream objects.
+ */
+
+@import Foundation;
+@import ExternalAccessory;
+
+extern NSString *EADSessionDataReceivedNotification;
+
+// NOTE: EADSessionController is not threadsafe, calling methods from different threads will lead to unpredictable results
+@interface EADSessionController : NSObject <EAAccessoryDelegate, NSStreamDelegate>
+
++ (EADSessionController *)sharedController;
+
+- (void)setupControllerForAccessory:(EAAccessory *)accessory withProtocolString:(NSString *)protocolString;
+
+- (BOOL)openSession;
+- (void)closeSession;
+
+- (void)writeData:(NSData *)data;
+
+- (NSUInteger)readBytesAvailable;
+- (NSData *)readData:(NSUInteger)bytesToRead;
+
+@property (nonatomic, readonly) EAAccessory *accessory;
+@property (nonatomic, readonly) NSString *protocolString;
+
+@end
